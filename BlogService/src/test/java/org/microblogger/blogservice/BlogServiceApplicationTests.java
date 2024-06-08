8eec54f7ea4465689b7a21d9775d0d35efee6737
sachinda96 <sachinda9696@gmail.com>
2024-06-08 20:07:34 +0200

    add services

---
+++ BlogService/src/test/java/org/microblogger/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microblogger.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ MediaService/src/test/java/org/microblogger/mediaservice/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.microblogger.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
